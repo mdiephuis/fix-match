2b90e769ea9a9cb3c4c27d52dc8e4c5de42bc545
Maurits Diephuis <mdiephuis@gmail.com>
2020-03-12 14:54:18 +0100

    boilerplate code

---
+++ data.py
@@ -0,0 +1,95 @@
+from torch.utils.data import DataLoader, Dataset
+from torchvision import datasets, transforms
+import numpy as np
+
+from PIL import ImageFilter
+from PIL import Image
+
+
+def cifar_strong_transforms():
+    all_transforms = transforms.Compose([
+        transforms.RandomResizedCrop(32),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+        transforms.RandomGrayscale(p=0.2),
+        transforms.ToTensor(),
+        transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
+    ])
+    return all_transforms
+
+
+def cifar_weak_transforms():
+    all_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
+    ])
+    return all_transforms
+
+
+def cifar_test_transforms():
+    all_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
+    ])
+    return all_transforms
+
+
+class CIFAR10C(datasets.CIFAR10):
+    def __init__(self, *args, **kwargs):
+        super(CIFAR10C, self).__init__(*args, **kwargs)
+
+    def __getitem__(self, index):
+        img, target = self.data[index], self.targets[index]
+
+        # return a PIL Image
+        img = Image.fromarray(img)
+
+        if self.transform is not None:
+            xi = self.transform(img)
+            xj = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return xi, xj, target
+
+
+class Loader(object):
+    def __init__(self, dataset_ident, file_path, download, batch_size, train_transform, test_transform, target_transform, use_cuda):
+
+        kwargs = {'num_workers': 4, 'pin_memory': True} if use_cuda else {}
+
+        loader_map = {
+            'CIFAR10C': CIFAR10C,
+            'CIFAR10': datasets.CIFAR10
+        }
+
+        num_class = {
+            'CIFAR10C': 10,
+            'CIFAR10': 10
+        }
+
+        # Get the datasets
+        train_dataset, test_dataset = self.get_dataset(loader_map[dataset_ident], file_path, download,
+                                                       train_transform, test_transform, target_transform)
+        # Set the loaders
+        self.train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, **kwargs)
+        self.test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, **kwargs)
+
+        tmp_batch = self.train_loader.__iter__().__next__()[0]
+        self.img_shape = list(tmp_batch.size())[1:]
+        self.num_class = num_class[dataset_ident]
+
+    @staticmethod
+    def get_dataset(dataset, file_path, download, train_transform, test_transform, target_transform):
+
+        # Training and Validation datasets
+        train_dataset = dataset(file_path, train=True, download=download,
+                                transform=train_transform,
+                                target_transform=target_transform)
+
+        test_dataset = dataset(file_path, train=False, download=download,
+                               transform=test_transform,
+                               target_transform=target_transform)
+
+        return train_dataset, test_dataset
---
+++ models.py
@@ -0,0 +1,114 @@
+import torch
+import torch.nn as nn
+from torch.hub import load_state_dict_from_url
+import torch.nn.functional as F
+
+
+class Bottleneck_CIFAR(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck_CIFAR, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class BasicBlock_CIFAR(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock_CIFAR, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet_CIFAR(nn.Module):
+
+    def __init__(self, block, layers, num_features=128):
+        super(ResNet_CIFAR, self).__init__()
+        self.in_planes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+
+        self.layer_block = nn.Sequential(
+            self._make_layer(block, 64, layers[0], stride=1),
+            self._make_layer(block, 128, layers[1], stride=2),
+            self._make_layer(block, 256, layers[2], stride=2),
+            self._make_layer(block, 512, layers[2], stride=2)
+        )
+
+        self.linear = nn.Linear(512 * block.expansion, num_features)
+
+        self.learning_head = nn.Sequential(
+            nn.Linear(512 * block.expansion, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Linear(512, num_features)
+        )
+
+    def _make_layer(self, block, planes, num_blocks, stride=1):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(self.bn1(x))
+
+        x = self.layer_block(x)
+
+        x = F.avg_pool2d(x, 4)
+
+        # Feature representation, 2048
+        h_out = x.view(x.size(0), -1)
+
+        # Learning output
+        z_out = self.learning_head(h_out)
+
+        return F.normalize(h_out, dim=-1), F.normalize(z_out, dim=-1)
+
+
+def resnet50_cifar(num_features=128):
+    return ResNet_CIFAR(Bottleneck_CIFAR, [3, 4, 6, 3], num_features)
+
+
+def resnet18_cifar(num_features=128):
+    return ResNet_CIFAR(BasicBlock_CIFAR, [2, 2, 2, 2], num_features)
