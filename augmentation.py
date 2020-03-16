9697e23bc590d4a5322ead5e4395b8fbdebbf340
diephuis <Maurits.Diephuis@unige.ch>
2020-03-16 11:19:01 +0100

    removed flips from test augmentation

--- augmentation.py
+++ augmentation.py
@@ -133,8 +133,6 @@ def cifar_weak_transforms():
 
 def cifar_test_transforms():
     transform_compose = transforms.Compose([
-        transforms.RandomHorizontalFlip(p=0.5),
-        transforms.RandomVerticalFlip(p=0.5),
         transforms.ToTensor(),
         transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
     ])
